800e80ed48f95b05faec752de491ebb295939c4a
ly.ton <dreadmoirai@gmail.com>
2017-01-14 21:24:52 -0800

    Update BotListener.java

--- src/dreadmoirai/samurais/BotListener.java
+++ src/dreadmoirai/samurais/BotListener.java
@@ -11,20 +11,21 @@ import java.util.HashMap;
  */
 public class BotListener extends ListenerAdapter {
 
-    public static HashMap identity;
+    public static HashMap response;
 
-    public BotListener() {
-        identity = new HashMap();
-        identity.put("[R:Shogun(267924909752188928)]","a 프로 게이머");
-        identity.put("[R:Daimyo(268458081225146369)]","ur ok");
-        identity.put("[R:Samurai(267925162991681547)]","cool");
-        identity.put("[R:Peasant(267924616574533634)]","meh");
+    public BotListener() { //constructor
+        response = new HashMap(); //hashmap for responses
+        response.put("[R:Shogun(267924909752188928)]","a 프로 게이머");
+        response.put("[R:Daimyo(268458081225146369)]","ur ok");
+        response.put("[R:Samurai(267925162991681547)]","cool");
+        response.put("[R:Peasant(267924616574533634)]","meh");
     }
 
     @Override
-    public void onMessageReceived(MessageReceivedEvent event) {
-        if(event.getMessage().getRawContent().equalsIgnoreCase("who am i?")) {
-            event.getChannel().sendMessage(event.getAuthor().getAsMention() + " " + identity.get(event.getMember().getRoles().toString())).queue();
+    public void onMessageReceived(MessageReceivedEvent event) {  //when a message is sent in the channel
+        String msg = event.getMessage().getRawContent().toLowerCase();
+        if(msg.equals("who am i?")) {
+            event.getChannel().sendMessage(event.getAuthor().getAsMention() + " " + response.get(event.getMember().getRoles().toString())).queue();
         }
     }
 
