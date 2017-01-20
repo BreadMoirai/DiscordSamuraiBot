598fbef619bf40f698b546d8b0fcee2dd9da5188
Mansfew <dreadmoirai@gmail.com>
2017-01-19 20:10:29 -0800

    Organized Code
    It is very clean now

--- src/dreadmoirais/samurais/BotListener.java
+++ src/dreadmoirais/samurais/BotListener.java
@@ -1,26 +1,25 @@
 package dreadmoirais.samurais;
 
+import net.dv8tion.jda.core.MessageBuilder;
 import net.dv8tion.jda.core.entities.ChannelType;
 import net.dv8tion.jda.core.entities.Message;
-import net.dv8tion.jda.core.entities.MessageEmbed;
+import net.dv8tion.jda.core.entities.Role;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.ReadyEvent;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+import java.util.function.Consumer;
 
 /**
  * Created by TonTL on 1/14/2017.
  * Handles events
  * API: http://home.dv8tion.net:8080/job/JDA/Promoted%20Build/javadoc/
- *
  */
 public class BotListener extends ListenerAdapter {
 
@@ -36,6 +35,9 @@ public class BotListener extends ListenerAdapter {
 
     private static Random rand;
 
+    private static List<Consumer<MessageReceivedEvent>> basicCommands;
+    private static List<Consumer<MessageReceivedEvent>> mentionCommands;
+
     /**
      * constructor
      */
@@ -46,6 +48,15 @@ public class BotListener extends ListenerAdapter {
 
         //Random object for rolls
         rand = new Random();
+        basicCommands = new ArrayList<>();
+        basicCommands.add(BotListener::getStat);
+        basicCommands.add(BotListener::getIdentity);
+        basicCommands.add(BotListener::getRoll);
+        basicCommands.add(BotListener::exitProtocol);
+        mentionCommands = new ArrayList<>();
+        mentionCommands.add(BotListener::getFlame);
+
+
     }
 
     @Override
@@ -60,55 +71,110 @@ public class BotListener extends ListenerAdapter {
     }
 
 
+    @Override
+    public void onMessageReactionAdd(MessageReactionAddEvent event) {
+        System.out.println(event.getReaction().getEmote().getName());
+    }
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         //fires when a message is sent in the channel
-
-        if (!event.isFromType(ChannelType.TEXT))
-        {  //If this message was not sent to a Guild TextChannel
-            event.getChannel().sendMessage("Boi why you dm me? Get the fuck outta here.");
-        } else {
-                if (event.getMessage().getMentionedUsers().contains(self)) {
-                    flame(event);
-                    //supports flame
-
-                } else if (filetransfer(event)) {
-                    //supports file
-
-                } else if (simpleResponse(event)) {
-                    //!stat
-                    //who am i?
-                    //!roll
-                } else if (exitProtocol(event)) {
-                    //!shutdown
-                    //!kys
-                }
-            }
-
-    }
-
-    private boolean flame(MessageReceivedEvent event) {//checks for @samurai
-        List<User> mentions = event.getMessage().getMentionedUsers();
-        if (mentions.contains(event.getJDA().getSelfUser())) {
-            if (event.getMessage().getRawContent().toLowerCase().contains("flame") && mentions.size() == 2) {
-                int killNumber = rand.nextInt(shadePhrases.size());
-                String killPhrase = shadePhrases.get(killNumber);
-                User target = mentions.get((1-mentions.indexOf(event.getJDA().getSelfUser())));
-                if (killNumber == 2) {//"[Member] more like stupid[(us)er]
-                    killPhrase += target.getName().substring(target.getName().length()/2);
-                }
-                killPhrase = String.format(killPhrase, target.getAsMention());
-                event.getChannel().sendMessage(killPhrase).queue();
-                data.addFlame(target.getId());
+        if (event.isFromType(ChannelType.TEXT)) {
+            if (event.getMessage().getMentionedUsers().contains(self)) {
+                for (Consumer<MessageReceivedEvent> command : mentionCommands)
+                    command.accept(event);
+            } else {
+                for (Consumer<MessageReceivedEvent> command : basicCommands)
+                    command.accept(event);
             }
         }
-        return false;
+    }
+
+    //Basic Commands
+    private static void getStat(MessageReceivedEvent event) {
+        if (event.getMessage().getContent().toLowerCase().contains("!stat")) {
+            if (event.getMessage().getMentionedUsers().size() == 0) {
+                event.getChannel().sendMessage(new InfoPanel(data.users.get(event.getAuthor().getId()))).queue();
+            } else {
+                for (User u : event.getMessage().getMentionedUsers()) {
+                    event.getChannel().sendMessage(new InfoPanel(data.users.get(u.getId()))).queue();
+                }
+            }
+        }
+    }
+
+    private static void getIdentity(MessageReceivedEvent event) {
+        if (event.getMessage().getContent().toLowerCase().contains(("who am i?"))) {
+            event.getChannel().sendMessage(new MessageBuilder()
+                    .append(event.getAuthor().getAsMention())
+                    .append(" ").append(roleResponses.get(event.getMember().getRoles().toString()))
+                    .build()).queue();
+        }
+    }
+
+    private static void getRoll(MessageReceivedEvent event) {
+        String message = event.getMessage().getContent().toLowerCase();
+        if (message.contains("!roll")) {
+            int x = 1;
+            if (message.length() < 7) {
+                x += rand.nextInt(100);
+            } else {
+                try {
+                    x += rand.nextInt(Integer.parseInt(message.trim().substring(6)));
+                } catch (NumberFormatException e) {
+                    x += rand.nextInt(100);
+                    event.getMessage().addReaction("\uD83D\uDE15").queue();
+                }
+            }
+            event.getChannel().sendMessage(new MessageBuilder()
+                    .append(event.getAuthor().getAsMention())
+                    .append(" rolled ")
+                    .append(x)
+                    .build()).queue();
+        }
+    }
+
+    private static void exitProtocol(MessageReceivedEvent event) {
+        if (event.getMessage().getContent().equalsIgnoreCase("!shutdown")) {
+            event.getChannel().sendMessage("sad boop").queue();
+            data.saveDataFull();
+            event.getJDA().shutdown();
+        }
+    }
+
+    //MentionCommands
+    private static void getFlame(MessageReceivedEvent event) {
+        if (event.getMessage().getContent().toLowerCase().contains("flame")) {
+            int upperBound = shadePhrases.size();
+            MessageBuilder mb = new MessageBuilder();
+            List<User> victims = new ArrayList<User>(event.getMessage().getMentionedUsers());
+            for (Role r : event.getMessage().getMentionedRoles()) {
+                event.getGuild().getMembers().stream().forEach(member -> {
+                    if (member.getRoles().contains(r)) {
+                        victims.add(member.getUser());
+                    }
+                });
+            }
+            for (User victim: victims) {
+                if (!victim.equals(self)) {
+                    mb.append(" ")
+                            .append(shadePhrases.get(rand.nextInt(upperBound)))
+                            .append("\n")
+                            .replaceAll("[victim]", victim.getAsMention());
+                    data.addFlame(victim.getId());
+                }
+            }
+            if (mb.isEmpty()) {
+                event.getMessage().addReaction("\uD83D\uDE15").queue();
+            } else {
+                event.getChannel().sendMessage(mb.build()).queue();
+            }
+        }
     }
 
     private boolean filetransfer(MessageReceivedEvent event) {
         List<Message.Attachment> attachments = event.getMessage().getAttachments();
-        if (attachments.size()>0) {
+        if (attachments.size() > 0) {
             System.out.println("\nFound Attachment.");
             for (Message.Attachment a : attachments) {
                 event.getChannel().sendMessage(String.format("Attachment Found.\n%s :%s bytes\n%s", a.getFileName(), a.getSize(), event.getMessage().getMentionedUsers().get(0).getName())).queue();
@@ -119,90 +185,32 @@ public class BotListener extends ListenerAdapter {
         return false;
     }
 
-    private boolean simpleResponse(MessageReceivedEvent event) {//basic responses
-        String messageReceived = event.getMessage().getRawContent().toLowerCase();
-        String messageSent = "";
-        if(messageReceived.contains("!stat")) {
-            getStat(event);
-        } else if (messageReceived.equals("who am i?")) {//who am i? response
-            messageSent = " " + roleResponses.get(event.getMember().getRoles().toString());
-        } else if (messageReceived.contains("!roll")) {//!roll response
-            if (messageReceived.length() > 6) {
-                try {
-                    int x = Integer.parseInt(messageReceived.substring(6));
-                    messageSent = " rolled " + (rand.nextInt(x) + 1) + "!";
-                } catch (NumberFormatException e) {
-                    messageSent = " what?";
-                }
-            } else {
-                messageSent = " rolled " + (rand.nextInt(100) + 1) + "!";
-            }
-        }
-        if (messageSent.length() > 0) {
-            event.getChannel().sendMessage(event.getAuthor().getAsMention() + messageSent).queue();
-            return true;
-        }
-        return false;
-    }
-
-    private List<MessageEmbed.Field> makeFieldList() {
-        List<MessageEmbed.Field> fields = new ArrayList<MessageEmbed.Field>();
-        fields.add(new MessageEmbed.Field("BodyA", "Lorem Ipsum a la mode", false));
-        fields.add(new MessageEmbed.Field("BodyB", "Body like it's drop", false));
-        return fields;
-    }
-
-    private void getStat(MessageReceivedEvent event) {
-        if (event.getMessage().getMentionedUsers().size()==0) {
-            event.getChannel().sendMessage(new InfoPanel(data.users.get(event.getAuthor().getId()))).queue();
-        } else {
-            for (User u: event.getMessage().getMentionedUsers()) {
-                event.getChannel().sendMessage(new InfoPanel(data.users.get(u.getId()))).queue();
-            }
-        }
-    }
-
-    private boolean exitProtocol(MessageReceivedEvent event) {
-        boolean shutdown = false;
-        if (event.getMessage().getRawContent().equalsIgnoreCase("!kys")) {
-            event.getChannel().sendMessage("ok :(").queue();
-            shutdown = true;
-        } else if (event.getMessage().getRawContent().equalsIgnoreCase("!shutdown")) {
-            event.getChannel().sendMessage("sad boop").queue();
-            shutdown = true;
-        }
-        if (shutdown) {
-            //save data?
-            data.saveDataFull();
-            event.getJDA().shutdown();
-        }
-        return false;
-    }
-
 
     private boolean duel(MessageReceivedEvent event) {
         String message = event.getMessage().getRawContent();
         List<User> mentions = event.getMessage().getMentionedUsers();
         if (event.getMessage().getRawContent() == null)
-        return false;
+            return false;
         return false;
     }
 
+    //miscMethods
+
     private void loadKeywords() {
-        try(BufferedReader br = new BufferedReader(new FileReader("src\\dreadmoirais\\data\\keywords.txt"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader("src\\dreadmoirais\\data\\keywords.txt"))) {
             String line = br.readLine();
             System.out.println("Reading " + line);
-            while (!(line=br.readLine()).equals("")) {
+            while (!(line = br.readLine()).equals("")) {
                 roleResponses.put(line, br.readLine());
             }
             line = br.readLine();
             System.out.println("Reading " + line);
-            while(!(line=br.readLine()).equals("")) {
+            while (!(line = br.readLine()).equals("")) {
                 fightWords.add(line);
             }
             line = br.readLine();
             System.out.println("Reading " + line);
-            while((line=br.readLine()) != null) {
+            while ((line = br.readLine()) != null) {
                 shadePhrases.add(line);
             }
 
