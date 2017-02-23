d509d43159a11f04322f4c864ea13565e6f7b89d
DreadMoirai <dreadmoirai@gmail.com>
2017-02-22 23:01:36 -0800

    Add 'plain' and 'noperm' arg to !invite
    Signed-off-by: DreadMoirai <dreadmoirai@gmail.com>

--- src/main/java/samurai/action/general/Invite.java
+++ src/main/java/samurai/action/general/Invite.java
@@ -14,7 +14,11 @@ public class Invite extends Action {
 
     @Override
     public SamuraiMessage buildMessage() {
-        String INVITE_URL = "https://discordapp.com/oauth2/authorize?client_id=270044218167132170&scope=bot&permissions=60480";
+
+        String INVITE_URL = "https://discordapp.com/oauth2/authorize?client_id=270044218167132170&scope=bot&permissions=126016";
+        if (args.size() == 1 && args.get(0).equalsIgnoreCase("plain") || args.get(0).equalsIgnoreCase("noperm")) {
+            return FixedMessage.build(INVITE_URL.substring(0, 78));
+        }
         return FixedMessage.build(INVITE_URL);
     }
 }
