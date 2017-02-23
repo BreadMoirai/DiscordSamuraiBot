92af8669fae8529fc6b9dc1a293da6de8277d27c
DreadMoirai <dreadmoirai@gmail.com>
2017-02-22 23:11:02 -0800

    Fixed logical flaw in !invite
    Signed-off-by: DreadMoirai <dreadmoirai@gmail.com>

--- src/main/java/samurai/action/general/Invite.java
+++ src/main/java/samurai/action/general/Invite.java
@@ -16,7 +16,7 @@ public class Invite extends Action {
     public SamuraiMessage buildMessage() {
 
         String INVITE_URL = "https://discordapp.com/oauth2/authorize?client_id=270044218167132170&scope=bot&permissions=126016";
-        if (args.size() == 1 && args.get(0).equalsIgnoreCase("plain") || args.get(0).equalsIgnoreCase("noperm")) {
+        if (args.size() == 1 && (args.get(0).equalsIgnoreCase("plain") || args.get(0).equalsIgnoreCase("noperm"))) {
             return FixedMessage.build(INVITE_URL.substring(0, 78));
         }
         return FixedMessage.build(INVITE_URL);
