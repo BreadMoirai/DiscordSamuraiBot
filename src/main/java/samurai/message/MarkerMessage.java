9c8f69dc33a2d1e6a69870b870371bd37fd30945
DreadMoirai <dreadmoirai@gmail.com>
2017-02-22 23:59:36 -0800

    Add Empty Markerclass to signal shutdown
    Signed-off-by: DreadMoirai <dreadmoirai@gmail.com>

---
+++ src/main/java/samurai/message/MarkerMessage.java
@@ -0,0 +1,14 @@
+package samurai.message;
+
+import net.dv8tion.jda.core.entities.Message;
+
+/**
+ * @author TonTL
+ * @version 4.x - 2/22/2017
+ */
+public class MarkerMessage extends SamuraiMessage {
+    @Override
+    public Message getMessage() {
+        return null;
+    }
+}
