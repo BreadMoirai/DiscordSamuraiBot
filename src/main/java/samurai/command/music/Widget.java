ca37174b8c49d9b81296f8969833fd59a5713545
DreadMoirai <DreadMoirai@gmail.com>
2017-04-18 01:23:36 -0700

    widget start

--- src/main/java/samurai/command/music/Widget.java
+++ src/main/java/samurai/command/music/Widget.java
@@ -1,8 +1,18 @@
 package samurai.command.music;
 
+import samurai.command.Command;
+import samurai.command.CommandContext;
+import samurai.command.annotations.Key;
+import samurai.messages.base.SamuraiMessage;
+
 /**
  * @author TonTL
  * @version 4/18/2017
  */
-public class Widget {
+@Key("musicwidget")
+public class Widget extends Command {
+    @Override
+    protected SamuraiMessage execute(CommandContext context) {
+        return null;
+    }
 }
