47f278aca3ed38558e1c38c154436b6ba11ef8ab
DreadMoirai <DreadMoirai@gmail.com>
2017-05-14 00:17:15 -0700

    Changed emojis to basic latin in HangmanGame

--- src/main/java/samurai/messages/impl/duel/HangmanGame.java
+++ src/main/java/samurai/messages/impl/duel/HangmanGame.java
@@ -45,8 +45,7 @@ import java.util.stream.IntStream;
 
 public class HangmanGame extends DynamicMessage implements PrivateMessageListener, ReactionListener, GenericCommandListener {
 
-    private static final List<String> HANGMAN_REACTIONS = Collections.unmodifiableList(Arrays.asList("?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?"));
-    //todo well this is borken
+    private static final List<String> HANGMAN_REACTIONS = Collections.unmodifiableList(Arrays.asList("\uD83C\uDDE6", "\uD83C\uDDE7", "\uD83C\uDDE8", "\uD83C\uDDE9", "\uD83C\uDDEA", "\uD83C\uDDEB", "\uD83C\uDDEC", "\uD83C\uDDED", "\uD83C\uDDEE", "\uD83C\uDDEF", "\uD83C\uDDF0", "\uD83C\uDDF1", "\uD83C\uDDF2", "\uD83C\uDDF3", "\uD83C\uDDF4", "\uD83C\uDDF5", "\uD83C\uDDF6", "\uD83C\uDDF7", "\uD83C\uDDF8", "\uD83C\uDDF9", "\uD83C\uDDFA", "\uD83C\uDDFB", "\uD83C\uDDFC", "\uD83C\uDDFD", "\uD83C\uDDFE", "\uD83C\uDDFF"));
     private static final List<String> HANGMAN_IMAGES = Collections.unmodifiableList(Arrays.asList("https://puu.sh/twqZM.jpg", "https://puu.sh/twr1V.jpg", "https://puu.sh/twr3V.jpg", "https://puu.sh/twrGc.jpg", "https://puu.sh/twrGx.jpg", "https://puu.sh/twrW2.jpg", "https://puu.sh/twrWn.jpg", "https://puu.sh/twrWJ.jpg", "https://puu.sh/twrWV.jpg", "https://puu.sh/twrX5.jpg", "https://puu.sh/twrXq.jpg"));
 
 
