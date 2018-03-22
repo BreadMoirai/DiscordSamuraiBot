69d35281c9fbcc0b81aef0dbbf14ea32aa87ae15
DreadMoirai <dreadmoirai@gmail.com>
2018-03-21 21:53:42 -0700

    Remove 73 unused classes replaced by BreadBotFramework

--- src/main/java/com/github/breadmoirai/samurai/messages/base/Reloadable.java
+++
@@ -1,9 +0,0 @@
-package com.github.breadmoirai.samurai.messages.base;
-
-import com.github.breadmoirai.samurai.SamuraiDiscord;
-
-import java.io.Serializable;
-
-public interface Reloadable extends Serializable {
-    void reload(SamuraiDiscord samuraiDiscord);
-}
